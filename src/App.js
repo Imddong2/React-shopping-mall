564da33c92ecf4ee3c435e16a7913bd66ca434e8
Imddong <hjinny9822@gmail.com>
2019-08-07 20:28:06 +0900

    create react app

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React,{Component} from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+class App extends Component{
+  render(){
+    return (
+      <div className="gray-background">
+       <img src={logo} lat="loco"/>
+       <h2>let's develop shopping mall system!</h2>
+      </div>
+    );
+
+  }
+}
+
+export default App;
